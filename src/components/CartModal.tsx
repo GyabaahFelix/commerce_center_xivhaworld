4cd05742d7f5010d908dd4456267e655ba880bd4
GyabaahFelix <felixic360@gmail.com>
2025-03-29 03:40:06 +0000

    took out  {/* Ignore TypeScript error for subtotal */}               {/* @ts-ignore */}

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -103,8 +103,7 @@ const CartModal = () => {
           <div className="">
             <div className="flex items-center justify-between font-semibold">
               <span className="">Subtotal</span>
-              {/* Ignore TypeScript error for subtotal */}
-              {/* @ts-ignore */}
+             
               <span className="">${cart.subtotal.amount}</span>
             </div>
             <p className="text-gray-500 text-sm mt-2 mb-4">
